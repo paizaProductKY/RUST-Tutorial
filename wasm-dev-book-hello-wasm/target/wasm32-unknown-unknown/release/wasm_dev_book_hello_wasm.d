de14cf3be9c954ad1f956771da152ffcd788b2d5
wanco0823 <wanco0823@gmail.com>
2023-08-15 16:51:45 +0900

    javascriptからWasmでビルドしたRustの関数を呼び出す

---
+++ wasm-dev-book-hello-wasm/target/wasm32-unknown-unknown/release/wasm_dev_book_hello_wasm.d
@@ -0,0 +1,1 @@
+C:\Git\ Repository\RUST-Tutorial\RUST-Tutorial\wasm-dev-book-hello-wasm\target\wasm32-unknown-unknown\release\wasm_dev_book_hello_wasm.wasm: C:\Git\ Repository\RUST-Tutorial\RUST-Tutorial\wasm-dev-book-hello-wasm\src\lib.rs
