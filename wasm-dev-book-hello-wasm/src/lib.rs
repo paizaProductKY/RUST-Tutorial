3530c704a31b49f06b8527f64494608ade069028
wanco0823 <wanco0823@gmail.com>
2023-08-15 17:18:38 +0900

    rustからjavascriptの関数を呼び出し/ サードパーティ製ライブラリを利用

--- wasm-dev-book-hello-wasm/src/lib.rs
+++ wasm-dev-book-hello-wasm/src/lib.rs
@@ -3,6 +3,32 @@ pub fn add(a: i32, b:i32) -> i32 {
     a + b
 }
 
+extern {
+    fn date_now() -> f64;
+}
+
+#[no_mangle]
+pub fn get_timestamp() -> f64 {
+    unsafe {
+        date_now()
+    }
+}
+
+extern crate tinymt;
+
+use tinymt::tinymt32;
+
+#[no_mangle]
+pub fn rand() -> u32 {
+    let param = tinymt32::Param {
+        mat1:0x8F7011EE,
+        mat2:0xFC78FF1F,
+        tmat:0x3793fdff,
+    };
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
