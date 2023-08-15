394979c358e182aee25ac909038aaede656d9244
wanco0823 <wanco0823@gmail.com>
2023-08-15 16:01:13 +0900

    cargo new hello_world

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Git Repository\RUST-Tutorial\RUST-Tutorial\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Git Repository\RUST-Tutorial\RUST-Tutorial\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
